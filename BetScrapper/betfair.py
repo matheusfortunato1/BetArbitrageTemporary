2853f7337199ac5f5ebb1625a81783e93ff6a1cc
matheus <matheusfortunatojb@gmail.com>
2020-11-30 23:47:02 -0300

    .

---
+++ BetScrapper/betfair.py
@@ -0,0 +1,78 @@
+import datetime
+
+import time
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+from datetime import datetime
+
+from mechanize import Browser
+
+class BetFair():
+    
+    
+    def __init__(self, url, filename, sample_time, n_attempts=2):
+        self.filename = filename
+        self.sample_time = sample_time
+        self.n_attempts = n_attempts
+        sleep_time = 5
+        self.url = url
+        self.br = Browser()
+        
+        while(True):
+            attempt = 0
+            for attempt in range(n_attempts):
+                try:
+                    self._load_page()
+                    self.write_games_odds()
+                    break
+                except:
+                    print('Erro!')
+                    time.sleep(sleep_time)
+            
+            time.sleep(self.sample_time-attempt*sleep_time)
+            
+        
+    def _load_page(self):
+        self.time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        page_content = self.br.open(self.url).read()
+        page = BeautifulSoup(page_content,"lxml")
+        page = page.find_all('div', class_='content-multipick')
+        self.page = page[0].find_all('div', class_='details-market market-3-runners')
+        
+    def _get_games_odds(self, pg):
+        
+        #TODO: tirar a porra desse slice 
+        start = str(pg.find_all('a')[0]).find('data-galabel')
+        end = str(pg.find_all('a')[0]).find('data-loader')
+        teams = str(pg.find_all('a')[0])[start+14:end-11].split(' x ')
+        team1 = teams[0]
+        team2 = teams[1]
+
+        x1 = str(pg.find_all('a')[1].find().get_text().replace('\n', ''))
+        x2 = str(pg.find_all('a')[2].find().get_text().replace('\n', ''))
+        x3 = str(pg.find_all('a')[3].find().get_text().replace('\n', ''))
+
+        return [team1, team2, x1, x2, x3]
+        
+    def write_games_odds(self):
+        for pg in self.page:
+            self._write_file(self._get_games_odds(pg))
+
+    def _write_file(self, line):
+        try:
+            with open(self.filename, "a") as f:
+                f.write('\n'+self.time)
+                for x in line:
+                    f.write(';'+ x)
+        except:
+            print('Criando arquivo: ' + self.filename)
+            with open(self.filename, "w") as f:
+                f.write('\n'+self.time)
+                for x in line:
+                    f.write(';'+ x)
+
+if __name__ == '__main__':
+    url = 'https://www.betfair.com/sport/football/brasil-serie-a/13'
+    BetFair(url, 'betfair20201130.txt', 45)                
---
+++ BetScrapper/betway.py
@@ -0,0 +1,72 @@
+import datetime
+import time
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+class Betway():
+    
+    
+    def __init__(self, url, filename, sample_time, n_attempts=2):
+        self.filename = filename
+        self.sample_time = sample_time
+        self.n_attempts = n_attempts
+        self.sleep_time = sample_time/5
+        self.url = url
+        self.driver = self._init_chromium_driver()
+        
+#         self._load_page()
+        while(True):
+            attempt = 0
+            for attempt in range(n_attempts):
+                try:
+                    self._load_page()
+                    print('Página carregou')
+                    self.write_games_odds()
+                    print('Foi escrito')
+                    break
+                except:
+                    print('Erro!')
+                    time.sleep(self.sleep_time)
+            
+            time.sleep(0.5*self.sample_time-attempt*self.sleep_time)
+            
+    def _init_chromium_driver(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--incognito')
+        options.add_argument('--headless')
+        return webdriver.Chrome("/Users/Matheus/Documents/chromedriver", options=options)
+            
+    def _load_page(self):
+        print('start...' + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        self.time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.driver.get(self.url)
+        time.sleep(self.sleep_time)
+        print('Get url ok ')
+        # Fucking sleep cause shit download speed
+        page_source = self.driver.page_source
+        time.sleep(self.sleep_time)
+        self.page = BeautifulSoup(page_source, "lxml")
+        
+    def _write_odds(self):
+        with open(self.filename, "a") as f:
+            for pg in self.page.find_all('div', class_='odds')[1:]:
+                f.write(str(pg.getText())+';')
+            f.write('\n')
+    
+    def _write_teams(self):
+        with open(self.filename, "a") as f:
+            print('Entrou em write teams')
+            f.write(self.time+';')
+            for pg in self.page.find_all('span', class_='teamNameEllipsisContainer'):
+                print(str(pg.find('span').getText())+';')
+                f.write(str(pg.find('span').getText())+';')
+            
+    def write_games_odds(self):
+        self._write_teams()
+        self._write_odds()
+
+if __name__ == '__main__':
+    Betway('https://sports.betway.com/pt/sports/grp/soccer/brazil/brasileiro-serie-a', 'betway20201130.txt', 60)
+    
